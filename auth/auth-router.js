f23ee42e5ce4a2e9071c10363004b75f34819158
Allie Robinson <allierob93@gmail.com>
2020-01-30 12:13:56 -0600

    GET to users is now restricted, MVP met

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,15 +25,12 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
     let { username, password } = req.body;
-    console.log({username, password})
-    console.log(username)
 
     Users.findByUsername(username)
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
                 const token = signToken(user);
-                console.log("token", token);
                 res.status(200).json({ token })
             } else {
                 res.status(401).json({ errorMessage: 'Invalid credentials.' });
--- users/users-model.js
+++ users/users-model.js
@@ -8,7 +8,7 @@ module.exports = {
 }
 
 function find() {
-    return db('users').select('id', 'username', 'department', 'password');
+    return db('users').select('id', 'username', 'department');
 };
 
 function add(user) {
